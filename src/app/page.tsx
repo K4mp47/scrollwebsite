baf2368d797e1aa812b754aca56bb6fd733b57a4
K4mp47 <campagnoloalberto5@gmail.com>
2025-04-22 10:07:07 +0200

    fix: new contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,253 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useRef, useState } from 'react';
+import Image from 'next/image';
+import Lenis from 'lenis';
+import { MotionValue, useScroll, useTransform, motion, useInView } from 'framer-motion';
+
+const images = [
+  "/hero.jpg",
+  "/hero2.jpg",
+  "/hero3.jpg",
+  "/hero4.jpg",
+  "/hero5.jpg",
+  "/hero6.jpg",
+  "/hero7.jpg",
+  "/hero8.jpg",
+  "/hero9.jpg",
+  "/fruits.jpg",
+  "/fruits2.jpg",
+  "/fruits3.jpg",
+  "/fruits4.jpg",
+  "/fruits5.jpg",
+];
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const gallery = useRef<HTMLDivElement>(null);
+  const [dimension, setDimension] = useState({ width: 0, height: 0 });
+  const title = useRef(null);
+  const contactTitle = useRef(null);
+  const isInView = useInView(title, { once: true, margin: '75%' });
+  const isContactInView = useInView(contactTitle, { 
+    once: true, 
+    margin: '-25%',
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const titleWords = "Software that works because we love making it.".split(" ");
+  const titleWords2 = "Contact Us!".split(" ");
+
+  const animation = {
+    initial: { y: "100%" },
+    enter: (i: number) => ({
+      y: "0",
+      transition: {
+        duration: 0.75,
+        ease: [0.33, 1, 0.68, 1],
+        delay: 0.075 * i
+      }
+    })
+  };
+
+  const animation2 = {
+    initial: { y: "100%" },
+    enter: (i: number) => ({
+      y: "0",
+      transition: {
+        duration: 0.75,
+        ease: [0.33, 1, 0.68, 1],
+        delay: 0.075 * i
+      }
+    })
+  }
+
+  const { scrollYProgress } = useScroll({
+    target: gallery,
+    offset: ['start end', 'end start'],
+  });
+  const { height } = dimension;
+  const y = useTransform(scrollYProgress, [0, 1], [0, height * 2]);
+  const y2 = useTransform(scrollYProgress, [0, 1], [0, height * 3.3]);
+  const y3 = useTransform(scrollYProgress, [0, 1], [0, height * 1.25]);
+  const y4 = useTransform(scrollYProgress, [0, 1], [0, height * 3]);
+  useEffect(() => {
+    const lenis = new Lenis();
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+    const resize = () => {
+      setDimension({ width: window.innerWidth, height: window.innerHeight });
+    };
+    window.addEventListener('resize', resize);
+    requestAnimationFrame(raf);
+    resize();
+    return () => {
+      window.removeEventListener('resize', resize);
+    };
+  }, []);
+  
+  return (
+    <main className="bg-white">
+      <div className="h-screen underline-hidden">
+        <h1 ref={title} className="text-[3rem] xl:text-[6rem] text-black font-mono font-black absolute top-1/5 left-0 pl-[3vw] uppercase md:top-1/4 md:left-0 md:w-3/5 flex flex-wrap">
+          {titleWords.map((word, i) => (
+            <span key={i} className="relative overflow-hidden mr-4 inline-block">
+              <motion.span
+                className="inline-block"
+                custom={i}
+                variants={animation}
+                initial="initial"
+                animate={isInView ? "enter" : ""}
+              >
+                {word}
+              </motion.span>
+            </span>
+          ))}
+        </h1>
+        <h2 className="text-md xl:text-[2rem] text-neutral-400 font-mono font-medium bottom-10 xl:bottom-12 px-[3vw] left-0 md:left-0 md:w-3/4 absolute">
+        Tailored software solutions, crafted with passion.
+        <motion.div
+          className="absolute bottom-3 lg:bottom-5 w-full md:flex justify-end hidden"
+          initial={{ y: 0 }}
+          animate={{ y: [0, 10, 0] }}
+          transition={{ repeat: Infinity, duration: 2 }}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 lg:h-8 lg:w-8" fill="none" viewBox="0 0 24 24" stroke="black">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
+          </svg>
+        </motion.div>
+        </h2>
+        
+      </div>
+      <div ref={gallery} className="h-[175vh] overflow-hidden bg-white border-t border-b border-neutral-200">
+        <div className="relative top-[-12.5vh] h-[120vh] lg:h-[200vh] flex gap-[2vw] p-[2vw]">
+          <Column images={[images[0], images[1], images[2]]} y={y} className="relative h-full min-w-full lg:w-1/4  lg:min-w-[250px] flex flex-col gap-[2vw] top-[-50%] lg:top-[-30%]" />
+          <Column images={[images[3], images[4], images[5]]} y={y2} className="relative h-full w-1/4 min-w-[250px] flex flex-col gap-[2vw] top-[-70%]" />
+          <Column images={[images[6], images[7], images[8]]} y={y3} className="relative h-full w-1/4 min-w-[250px] flex flex-col gap-[2vw] top-[-30%]" />
+          <Column images={[images[9], images[10], images[11]]} y={y4} className="relative h-full w-1/4 min-w-[250px] flex flex-col gap-[2vw] top-[-60%]" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="h-screen flex items-center">
+        <div className="w-full flex flex-col lg:flex-row items-center justify-end">
+          <div ref={contactTitle} className="text-left mb-8 lg:mb-0 w-full lg:w-1/2 mt-10 flex flex-col items-start">
+            <div className="flex flex-wrap">
+              {titleWords2.map((word, i) => (
+                <span key={i} className="text-[3rem] pl-[3vw] uppercase xl:text-[6rem] text-black font-mono font-black mb-0 overflow-hidden mr-4 inline-block text-center">
+                  <motion.span
+                    className="inline-block"
+                    custom={i}
+                    variants={animation2}
+                    initial="initial"
+                    animate={isContactInView ? "enter" : ""}
+                  >
+                    {word}
+                  </motion.span>
+                </span>
+              ))}
+            </div>
+            <h2 className="text-md pl-[3vw] xl:text-[2rem] text-neutral-400 font-mono font-medium mt-2">
+              <div className='flex items-center mb-0'>
+                contact@gmail.com 
+                <svg className="w-4 h-4 md:w-[1.5rem] md:h-[1.5rem] ml-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" /></svg>
+              </div>
+              For your next project :)
+            </h2>
+          </div>
+          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end pr-[3vw]">
+            <pre className="text-xs sm:text-sm text-neutral-500 font-mono scale-75 md:scale-100">
+{`                                 __
+                     .--.      .'  \`.
+                   .' . :\\    /   :  L
+                   F     :\\  /   . : |        .-._
+                  /     :  \\/        J      .' ___\\
+                 J     :   /      : : L    /--'   \`\`.
+                 F      : J           |  .<'.o.  \`-'>
+                /        J             L \\_>.   .--w)
+               J        /              \\_/|   . \`-__|
+               F                        / \`    -' /|)
+              |   :                    J   '        |
+             .'   ':                   |    .    :  \\
+            /                          J      :     |L
+           F                              |     \\   ||
+          F .                             |   :      |
+         F  |                             ; .   :  : F
+        /   |                                     : J
+       J    J             )                ;        F
+       |     L           /      .:'                J
+    .-'F:     L        ./       :: :       .       F
+    \`-'F:     .\\    \`:.J         :::.             J
+      J       :\\    \`:|        |::::\\            |
+      J        |:\`.    J        :\`:::\\            F
+       L   :':/ \\ \`-\`.  \\       : \`:::|        .-'
+       |     /   L    >--\\         :::|\`.    .-'
+       J    J    |    |   L     .  :::: :\`, /
+        L   F    J    )   |        >::   : /
+        |  J      L   F   \\     .-.:'   . /
+        ): |     J   /     \`-   | |   .--'
+        /  |     |: J        L  J J   )
+        L  |     |: |        L   F|   /
+        \\: J     \\:  L       \\  /  L |
+         L |      \\  |        F|   | )
+         J F       \\ J       J |   |J
+          L|        \\ \\      | |   | L
+          J L        \\ \\     F \\   F |
+           L\\         \\ \\   J   | J   L
+          /__\\_________)_\`._)_  |_/   \\_____
+                              ""   \`"""`}
+{/* {`
+                      .,,uod8B8bou,,.
+              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.
+         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||
+         !...:!TVBBBRPFT||||||||||!!^^""'   ||||
+         !.......:!?|||||!!^^""'            ||||
+         !.........||||                     ||||
+         !.........||||  ##                 ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         \.........||||                    ,||||
+          .;.......||||               _.-!!|||||
+   .,uodWBBBBb.....||||       _.-!!|||||||||!:'
+!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....
+!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   \\.
+!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     \\.
+!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';:::       \\.
+!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.
+\\..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
+  \\..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
+    \\........::::::::::::::::;iof688888888888888888888b.     '
+      \\......:::::::::;iof688888888888888888888888888888b.
+        \\....:::;iof688888888888888888888888888888888888b.
+          \\..::!8888888888888888888888888888888899fT!^"'
+            \\' !!988888888888888888888888888899fT|!^"'
+                \\!!8888888888888888899fT|!^"'
+                  \\!988888888899fT|!^"'
+                    \\!9899fT|!^"'
+                      \\!^"'
+`} */}
+            </pre>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+const Column = ({ images, y, className }: { images: string[]; y: MotionValue<number>; className?: string }) => {
+  return (
+    <motion.div
+      className={`${className}`}
+      style={{ y }}
+    >
+      {images.map((src, i) => (
+        <div key={i} className="relative h-1/3 w-full overflow-hidden">
+          <div className="relative w-full h-full">
+            <Image src={`${src}`} alt={`image ${i}`} fill className="object-cover rounded-md" />
+            <div className="w-full h-full bg-neutral-200 rounded-md" />
+            <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity duration-300 rounded-md" />
+          </div>
+        </div>
+      ))}
+    </motion.div>
   );
 }
